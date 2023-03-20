8aae8cb0c1b7b1e220b0e88798372d5405d29975
Alicia Virreira <90225797+AliciaV15@users.noreply.github.com>
2023-03-19 23:47:40 -0400

    Images carrusel for the info totem view

---
+++ src/components/Card.jsx
@@ -0,0 +1,24 @@
+import styles from "./Card.module.css";
+
+export function Card() {
+  return (
+    <div className={styles.panel}>
+      
+      <h1>How did we do?</h1>
+      <p>
+      Lorem Ipsum is simply dummy text of the printing and
+       typesetting industry. Lorem Ipsum has been the industry's
+    standard dummy text ever since the 1500s. sdfsdf
+     
+      </p>
+
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>4</button>
+      <button>5</button>
+
+      <button>Sumit</button>
+    </div>
+  );
+}
---
+++ src/components/Carrusel.jsx
@@ -0,0 +1,40 @@
+
+import { useEffect, useState } from "react";
+import "../style/Carrusel.css";
+
+function Carrusel({ images }) {
+    const [current, setCurrent] = useState(0);
+    const [autoPlay, setAutoPlay] = useState(null);
+    useEffect(()=>{
+        setTimeout(() => {
+            slideRight()
+
+        }, 4000); 
+     });
+     const slideRight = () => {
+        setCurrent(current == images.length - 1 ? 0: current + 1);
+     }
+  return (
+    <div className="carrusel">
+      <div className="carrusel_wrapper">
+        {images.map((image, index) => {
+          return (
+            <div
+              key={index}
+              className={
+                index == current
+                  ? "carrusel_card carrusel_card_active"
+                  : "carrusel_card"
+              }
+            >
+              <img className="card_image" src={image.image} alt="" />
+            </div>
+          );
+        })}
+        
+      </div>
+    </div>
+  );
+}
+
+export default Carrusel;
