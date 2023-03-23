1c2239bc5d8f0ec1dc174cf7cfa530e8223a1ac2
NicolasMenesesB <NicolasMenesesB>
2023-03-22 21:26:44 -0400

    Publicidad_Carrusel_NicolasMeneses

---
+++ src/components/Carrusel.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import "../style/Carrusel.css";
+
+function Carrusel({ images }) {
+    const [current, setCurrent] = useState(0);
+    const [autoPlay, setAutoPlay] = useState(null);
+    useEffect(()=>{
+        setTimeout(() => {
+            slideRight()
+
+        }, 10000); 
+     });
+     const slideRight = () => {
+        setCurrent(current == images.length - 1 ? 0: current + 1);
+     }
+  return (
+    <div className="carrusel">
+      <div className="carrusel_wrapper_publicidad">
+        {images.map((image, index) => {
+          return (
+            <div
+              key={index}
+              className={
+                index == current
+                  ? "carrusel_card_publicidad carrusel_card_active_publicidad"
+                  : "carrusel_card_publicidad"
+              }
+            >
+              <img className="card_image_publicidad" src={image.image} alt="" />
+            </div>
+          );
+        })}
+        
+      </div>
+    </div>
+  );
+}
+
+export default Carrusel;
