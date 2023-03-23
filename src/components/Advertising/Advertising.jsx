a547d7041ebb1f17266634bc3074e3943549e45e
FernandoTFW <fernando.aparicio1811@gmail.com>
2023-03-22 20:16:10 -0400

    Se ha hecho el componente de la publicidad

---
+++ src/components/Advertising/Advertising.jsx
@@ -0,0 +1,16 @@
+import './styleCard.css'
+export function Advertising(props) {
+  return <div class="card">
+      <div class="expDate">
+          <div class="expText">
+              <div>Vcto: {props.date}</div>
+          </div>
+      </div>
+      <div class="imagePart">
+          <img class="imgAdd" src={props.src} alt=""/>
+      </div>
+      <button class="deleteButton" onclick="">
+          X
+      </button>
+  </div>;
+}
