b047a12b790aa243e2aa1a1e401febc27ed7a85b
LucianaMayraBlancoAranibar <bal0029274@est.univalle.edu>
2023-04-12 19:47:02 -0400

    input para varias imagenes lba

--- src/components/Locacion.jsx
+++ src/components/Locacion.jsx
@@ -69,15 +69,15 @@ const Locacion = () => {
               </label>
               <input
                 class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                aria-describedby="file_input_help"
-                id="file_input"
+                id="multiple_files"
                 type="file"
+                multiple
               />
               <p
                 class="mt-1 text-sm text-gray-500 dark:text-gray-300"
                 id="file_input_help"
               >
-                SVG, PNG, JPG or GIF (MAX. 800x400px).
+                SVG, PNG, JPG or GIF .
               </p>
             </div>
 
@@ -90,15 +90,15 @@ const Locacion = () => {
               </label>
               <input
                 class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                aria-describedby="file_input_help"
-                id="file_input"
+                id="multiple_files"
                 type="file"
+                multiple
               />
               <p
                 class="mt-1 text-sm text-gray-500 dark:text-gray-300"
                 id="file_input_help"
               >
-                SVG, PNG, JPG or GIF (MAX. 800x400px).
+                SVG, PNG, JPG or GIF .
               </p>
             </div>
 
