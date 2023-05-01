3c7c59bcfb349a66c037ea5255234052d4102231
NicolasMenesesB <NicolasMenesesB>
2023-04-30 23:16:29 -0400

    NicolasMeneses_LocaiconCRUD

--- src/views/Forms/Locacion.jsx
+++ src/views/Forms/Locacion.jsx
@@ -1,157 +1,72 @@
-import React from "react";
+import React, { useState } from 'react';
+import axios from 'axios';
 
-const Locacion = () => {
+function Locacion() {
+  const [nombre, setNombre] = useState('');
+  const [descripcion, setDescripcion] = useState('');
+  const [keywords, setKeywords] = useState('');
+  const [idTotem, setIdTotem] = useState('');
+  const [imagenMapa, setImagenMapa] = useState(null);
+  const [imagenesCarrucel, setImagenesCarrucel] = useState(null);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append('nombre', nombre);
+    formData.append('descripcion', descripcion);
+    formData.append('keywords', keywords);
+    formData.append('idTotem', idTotem);
+    formData.append('imagenMapa', imagenMapa);
+    formData.append('imagenesCarrucel', imagenesCarrucel);
+
+    try {
+      const response = await axios.post('https://localhost:7264/api/Locaciones', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <>
-      <section class="max-w-full h-full p-6 bg-white rounded-md shadow-md dark:bg-gray-800 mt-0">
-        <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-            Nueva Localizacion
-          </p>
-        </div>
-
-        <form>
-          <div>
-            <input
-              id="username"
-              type="text"
-              placeholder="Nombre"
-              class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-            />
-          </div>
-
-          <div>
-            <input
-              id="description"
-              type="text"
-              placeholder="Descripcion"
-              class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-            />
-          </div>
-          <div class="">
-            <div>
-              <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
-                <div>
-                  <input
-                    id="clave"
-                    type="text"
-                    placeholder="Palabra clave"
-                    class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-                  />
-                </div>
-                <div>
-                  <button class="p-0 w-12 h-12 bg-red-600 rounded-full hover:bg-red-700 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none">
-                    <svg
-                      viewBox="0 0 20 20"
-                      enable-background="new 0 0 20 20"
-                      class="w-7 h-7 inline-block"
-                    >
-                      <path
-                        fill="#FFFFFF"
-                        d="M16,10c0,0.553-0.048,1-0.601,1H11v4.399C11,15.951,10.553,16,10,16c-0.553,0-1-0.049-1-0.601V11H4.601
-                                    C4.049,11,4,10.553,4,10c0-0.553,0.049-1,0.601-1H9V4.601C9,4.048,9.447,4,10,4c0.553,0,1,0.048,1,0.601V9h4.399
-                                    C15.952,9,16,9.447,16,10z"
-                      />
-                    </svg>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <br></br>
-          <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-3">
-            <div>
-              <label
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                for="file_input"
-              >
-                Subir imagen
-              </label>
-              <input
-                class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                id="multiple_files"
-                type="file"
-                multiple
-              />
-              <p
-                class="mt-1 text-sm text-gray-500 dark:text-gray-300"
-                id="file_input_help"
-              >
-                SVG, PNG, JPG or GIF .
-              </p>
-            </div>
-
-            <div>
-              <label
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                for="file_input"
-              >
-                Subir ubicacion
-              </label>
-              <input
-                class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                id="multiple_files"
-                type="file"
-                multiple
-              />
-              <p
-                class="mt-1 text-sm text-gray-500 dark:text-gray-300"
-                id="file_input_help"
-              >
-                SVG, PNG, JPG or GIF .
-              </p>
-            </div>
-
-            <div>
-              <div class="relative overflow-x-auto">
-                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                  <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    <tr>
-                      <th scope="col" class="px-6 py-3">
-                        Palabras Claves
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                      <th
-                        scope="row"
-                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                      >
-                        Iglesia
-                      </th>
-                    </tr>
-                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                      <th
-                        scope="row"
-                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                      >
-                        Iglesia Principal
-                      </th>
-                    </tr>
-                    <tr class="bg-white dark:bg-gray-800">
-                      <th
-                        scope="row"
-                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                      >
-                        Iglesia San Pedro
-                      </th>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-
-          <div class="flex justify-center mt-6">
-            <button class="px-6 py-2 leading-5 text-black transition-colors duration-200 transform bg-white rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
-              Añadir
-            </button>
-          </div>
-        </form>
-      </section>
-    </>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Nombre:
+        <input type="text" value={nombre} onChange={(event) => setNombre(event.target.value)} />
+      </label>
+      <br />
+      <label>
+        Descripción:
+        <textarea value={descripcion} onChange={(event) => setDescripcion(event.target.value)}></textarea>
+      </label>
+      <br />
+      <label>
+        Palabras clave:
+        <input type="text" value={keywords} onChange={(event) => setKeywords(event.target.value)} />
+      </label>
+      <br />
+      <label>
+        ID Totem:
+        <input type="text" value={idTotem} onChange={(event) => setIdTotem(event.target.value)} />
+      </label>
+      <br />
+      <label>
+        Imagen de mapa:
+        <input type="file" onChange={(event) => setImagenMapa(event.target.files[0])} />
+      </label>
+      <br />
+      <label>
+        Imágenes de carrusel:
+        <input type="file" multiple onChange={(event) => setImagenesCarrucel(event.target.files)} />
+      </label>
+      <br />
+      <button type="submit">Guardar</button>
+    </form>
   );
-};
+}
 
 export default Locacion;
