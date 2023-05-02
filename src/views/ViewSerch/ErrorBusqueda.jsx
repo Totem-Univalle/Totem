e726e464b7490c2a9b464d4bfda46e8893a5e875
NicolasMenesesB <NicolasMenesesB>
2023-05-01 21:36:33 -0400

    NicolasMeneses_ErrorBusqueda

---
+++ src/views/ViewSerch/ErrorBusqueda.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+function VentanaModal() {
+  const [open, setOpen] = useState(false);
+  const [gifUrl, setGifUrl] = useState('https://media.tenor.com/a4SaQmo5EesAAAAj/iranserver-bluebot.gif');
+  const [mensaje, setMensaje] = useState('Lo siento no encontramos resultados en su busqueda, Por favor intente nuevamente');
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+        <div>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleClick}>
+                Mostrar ventana de error
+            </button>
+        {open && (
+            <div className="fixed z-10 inset-0 overflow-y-auto">
+            <div className="flex items-center justify-center min-h-screen pt-2 px-2 pb-20 text-center sm:block sm:p-0">
+                <div className="fixed inset-0 transition-opacity">
+                <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+                </div>
+                <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+                <div className="inline-block align-bottom bg-blue rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                    <img className="mx-auto h-48" src={gifUrl} alt="gif" />
+                    <p className="text-center">{mensaje}</p>
+                </div>
+                <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                    <button onClick={handleClose} className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm">
+                    Cerrar
+                    </button>
+                </div>
+                </div>
+            </div>
+            </div>
+        )}
+        </div>
+    </>
+  );
+}
+
+export default VentanaModal;
