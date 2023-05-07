cd7b60a99e7159b1b988f2cc2108315496a0edc9
NicolasMenesesB <NicolasMenesesB>
2023-05-07 15:38:26 -0400

    ARREGLOS

--- src/views/ViewSerch/Buscando.jsx
+++ src/views/ViewSerch/Buscando.jsx
@@ -31,6 +31,9 @@ function VentanaModal() {
                     <img className="mx-auto h-48" src={gifUrl} alt="gif" />
                     <p className="text-center">{mensaje}</p>
                 </div>
+                <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex ">
+                  <input className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"></input>
+                </div>
                 <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                     <button onClick={handleClose} className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm">
                     Cerrar
