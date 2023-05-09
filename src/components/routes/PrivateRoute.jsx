9eadc1fa5d771b3ae04ddf3e2707e4e799367426
Hilay <al.mc8k@gmail.com>
2023-05-08 23:35:58 -0400

    Login Bug

--- src/components/routes/PrivateRoute.jsx
+++ src/components/routes/PrivateRoute.jsx
@@ -1,7 +1,6 @@
 import { Navigate, Outlet } from "react-router-dom";
-const isAllowed = false;
+const isAllowed = localStorage.getItem("token");
 export const PrivateRoute = ({
-  isAllowed,
   redirectTo = "/Login",
   children,
 }) => {
