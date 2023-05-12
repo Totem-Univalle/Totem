2d0f64f3846158fbeea2f417de5cd08e2dabdb33
LucianaMayraBlancoAranibar <bal0029274@est.univalle.edu>
2023-05-11 23:57:11 -0400

    EleccionNroPlantila_Totem

--- src/views/Forms/SeleccionPlantilla.jsx
+++ src/views/Forms/SeleccionPlantilla.jsx
@@ -46,7 +46,7 @@ const SeleccionPlantilla = () => {
     <div name="portfolio" className="">
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+          <p className="text-4xl font-bold inline text-justify border-b-4 border-gray-500">
             Plantillas
           </p>
           <p className="py-6">Seleccione la plantilla de su preferencia</p>
